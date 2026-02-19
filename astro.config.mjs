5440523c1df4f41e274fcb491599b668ec1952f3
02loveslollipop <02loveslollipop@users.noreply.github.com>
2026-02-19 17:51:10 -0500

    feat: integrate sitemap support and update site URL to ch0wn3rs.ninja

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ch0wn3rs.netlify.app',
+  site: 'https://ch0wn3rs.ninja/',
   base: '/',
   output: 'server',
   adapter: vercel({
     edgeMiddleware: true,
   }),
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/api/'),
+    }),
+  ],
 });
